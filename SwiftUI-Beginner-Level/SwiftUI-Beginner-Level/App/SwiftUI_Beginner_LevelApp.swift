236809c7e2e14eb2cfd2dbf6b49d7172c4275666
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-02-11 12:12:24 +0200

    Set up a project

---
+++ SwiftUI-Beginner-Level/SwiftUI-Beginner-Level/App/SwiftUI_Beginner_LevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Beginner_LevelApp.swift
+//  SwiftUI-Beginner-Level
+//
+//  Created by Mykyta Kuzminov on 11.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Beginner_LevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
