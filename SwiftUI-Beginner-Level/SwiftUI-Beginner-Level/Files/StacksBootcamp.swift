00d14b1c525526f7aa187988b351546cada20f6c
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-02-11 13:58:09 +0200

    Stacks

---
+++ SwiftUI-Beginner-Level/SwiftUI-Beginner-Level/Files/StacksBootcamp.swift
@@ -0,0 +1,48 @@
+//
+//  StacksBootcamp.swift
+//  SwiftUI-Beginner-Level
+//
+//  Created by Mykyta Kuzminov on 11.02.2024.
+//
+
+import SwiftUI
+
+struct StacksBootcamp: View {
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            Rectangle()
+                .fill(Color.yellow)
+                .frame(width: 350, height: 500, alignment: .center)
+            
+            VStack(alignment: .leading, spacing: 30) {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 150, height: 150)
+                
+                Rectangle()
+                    .fill(Color.green)
+                    .frame(width: 100, height: 100)
+                
+                HStack(alignment: .bottom) {
+                    Rectangle()
+                        .fill(Color.purple)
+                        .frame(width: 50, height: 50)
+                    
+                    Rectangle()
+                        .fill(Color.pink)
+                        .frame(width: 75, height: 75)
+                    
+                    Rectangle()
+                        .fill(Color.blue)
+                        .frame(width: 25, height: 25)
+                }
+                .background(Color.white)
+            }
+            .background(Color.black)
+        }
+    }
+}
+
+#Preview {
+    StacksBootcamp()
+}
