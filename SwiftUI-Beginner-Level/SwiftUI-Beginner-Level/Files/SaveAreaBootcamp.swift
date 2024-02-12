502f4ec05d4df29cb4ede9d57a7c6aaf57c31c58
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-02-12 09:49:54 +0200

    Safe area

---
+++ SwiftUI-Beginner-Level/SwiftUI-Beginner-Level/Files/SaveAreaBootcamp.swift
@@ -0,0 +1,25 @@
+//
+//  SaveAreaBootcamp.swift
+//  SwiftUI-Beginner-Level
+//
+//  Created by Mykyta Kuzminov on 12.02.2024.
+//
+
+import SwiftUI
+
+struct SaveAreaBootcamp: View {
+    var body: some View {
+        ZStack {
+            Color.blue.ignoresSafeArea()
+        
+            VStack {
+                Text("Hello, World!")
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    SaveAreaBootcamp()
+}
