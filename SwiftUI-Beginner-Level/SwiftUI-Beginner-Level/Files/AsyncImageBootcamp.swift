8e307566383db406a5c18db5dfd904298c385f03
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-02-16 18:06:38 +0200

    AsyncImage

---
+++ SwiftUI-Beginner-Level/SwiftUI-Beginner-Level/Files/AsyncImageBootcamp.swift
@@ -0,0 +1,32 @@
+//
+//  AsyncImageBootcamp.swift
+//  SwiftUI-Beginner-Level
+//
+//  Created by Mykyta Kuzminov on 16.02.2024.
+//
+
+import SwiftUI
+
+struct AsyncImageBootcamp: View {
+    
+    let url = URL(string: "https://picsum.photos/200")
+    
+    var body: some View {
+        AsyncImage(url: url) { returnedImage in
+            returnedImage
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+                .clipShape(.rect(cornerRadius: 20))
+                .shadow(radius: 10)
+            
+        } placeholder: {
+            ProgressView()
+        }
+            
+    }
+}
+
+#Preview {
+    AsyncImageBootcamp()
+}
